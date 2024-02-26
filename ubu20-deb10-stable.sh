a6cc9b4384d53a0105d51f07246af743858afeb1
ALAWI-VPN <159485644+ALAWI-VPN@users.noreply.github.com>
2024-02-26 09:58:56 +0700

    Update ubu20-deb10-stable.sh

--- ubu20-deb10-stable.sh
+++ ubu20-deb10-stable.sh
@@ -26,26 +26,6 @@ clear;clear;clear
 echo -e ""
 echo -e "AUTOSCRIPT MODIFIED BY ALAWI VPN"
 sleep 2
-###### IZIN SC 
-checking_sc() {
-  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
-  if [[ $date_list < $useexp ]]; then
-    echo -ne
-  else
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e ""
-    echo -e "            ${RED}PERMISSION DENIED !${NC}"
-    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
-    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-    echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6283890977190"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    exit 0
-  fi
-}
-checking_sc
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 # // Checking Os Architecture
