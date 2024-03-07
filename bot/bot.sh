167ad84b23122127acee14913e82cc33ae962065
ALAWI-VPN <159485644+ALAWI-VPN@users.noreply.github.com>
2024-03-07 08:42:41 +0700

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-NS=$( cat /etc/xray/dns )
-PUB=$( cat /etc/slowdns/server.pub )
 domain=$(cat /etc/xray/domain)
 #color
 grenbo="\e[92;1m"
@@ -9,17 +7,17 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/ALAWI-VPN/autoscript/main/bot/bot.zip
+wget https://raw.githubusercontent.com/ALAWI-VPN/VPN/vip/umdah/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/ALAWI-VPN/autoscript/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/ALAWI-VPN/VPN/vip/umdah/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
-clear
+#isi data
 echo ""
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
@@ -28,14 +26,11 @@ echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
 echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
 echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
 read -e -p "[*] Input your Bot Token : " bottoken
 read -e -p "[*] Input Your Id Telegram :" admin
 echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
 echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
 echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
-echo -e PUB='"'$PUB'"' >> /usr/bin/kyt/var.txt
-echo -e HOST='"'$NS'"' >> /usr/bin/kyt/var.txt
 clear
 
 cat > /etc/systemd/system/kyt.service << END
@@ -60,14 +55,11 @@ rm -rf kyt.sh
 echo "Done"
 echo "Your Data Bot"
 echo -e "==============================="
-echo "Token Bot      : $bottoken"
-echo "Admin          : $admin"
-echo "Domain         : $domain"
-echo "Pub            : $PUB"
-echo "Host           : $NS"
+echo "Token Bot     : $bottoken"
+echo "Admin         : $admin"
+echo "Domain        : $domain"
 echo -e "==============================="
 echo "Setting done"
-sleep 2
 clear
 
 echo " Installations complete, type /menu on your bot"
