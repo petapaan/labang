723c753effeed49c05479fa29c8256bbf3ca7ead
ALAWI-VPN <159485644+ALAWI-VPN@users.noreply.github.com>
2024-03-15 18:47:32 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#warna-warna
+biru="\033[1;36m"
+hijau="\e[92;1m"
+green='\e[32m'
+red='\e[38;5;208m'
+NC='\e[0m'
+clear
 #random
 apt install jq curl -y
 rm -rf /root/xray/scdomain
@@ -8,44 +15,27 @@ echo ""
 echo ""
 echo ""
 #sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
-read -rp "Input Domain Name. Example ( alawivpn ): " -e sub
-DOMAIN="cakdayat.my.id"
-SUB_DOMAIN="${sub}.cakdayat.my.id"
-SUB_DOMAIN2="*.$SUB_DOMAIN"
+read -rp "$(echo -e "${red}Masukkan ${hijau}subdomain ${NC}kamu: ")" sub
+# akun cloudflare
 CF_ID="vpsvpsku@gmail.com"
 CF_KEY="cb9b858e75a955df979cf4bff74839df1943d"
-set -euo pipefail
-IP=$(curl -sS ifconfig.me);
-echo "Updating DNS for ${SUB_DOMAIN}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
--H "X-Auth-Email: ${CF_ID}" \
--H "X-Auth-Key: ${CF_KEY}" \
--H "Content-Type: application/json" | jq -r .result[0].id)
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
--H "X-Auth-Email: ${CF_ID}" \
--H "X-Auth-Key: ${CF_KEY}" \
--H "Content-Type: application/json" | jq -r .result[0].id)
-if [[ "${#RECORD}" -le 10 ]]; then
-RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
--H "X-Auth-Email: ${CF_ID}" \
--H "X-Auth-Key: ${CF_KEY}" \
--H "Content-Type: application/json" \
---data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
-fi
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
--H "X-Auth-Email: ${CF_ID}" \
--H "X-Auth-Key: ${CF_KEY}" \
--H "Content-Type: application/json" \
---data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 
-# WILDCARD
-set -euo pipefail
+# Detail Record A
+DOMAIN1="cakdayat.my.id"
+SUB_DOMAIN1="${sub}.${DOMAIN1}"
+SUB_DOMAIN01="*.${SUB_DOMAIN1}"
 IP=$(curl -sS ifconfig.me)
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+
+# SUB 1
+set -euo pipefail
+IP1=${IP};
+echo -e ""
+echo -e "Sedang pointing ${biru}${SUB_DOMAIN1}$NC..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN1}&status=active" \
 -H "X-Auth-Email: ${CF_ID}" \
 -H "X-Auth-Key: ${CF_KEY}" \
 -H "Content-Type: application/json" | jq -r .result[0].id)
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN2}" \
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN1}" \
 -H "X-Auth-Email: ${CF_ID}" \
 -H "X-Auth-Key: ${CF_KEY}" \
 -H "Content-Type: application/json" | jq -r .result[0].id)
@@ -54,21 +44,49 @@ RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_
 -H "X-Auth-Email: ${CF_ID}" \
 -H "X-Auth-Key: ${CF_KEY}" \
 -H "Content-Type: application/json" \
---data '{"type":"A","name":"'${SUB_DOMAIN2}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+--data '{"type":"A","name":"'${SUB_DOMAIN1}'","content":"'${IP1}'","ttl":120,"proxied":false}' | jq -r .result.id)
 fi
 RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
 -H "X-Auth-Email: ${CF_ID}" \
 -H "X-Auth-Key: ${CF_KEY}" \
 -H "Content-Type: application/json" \
---data '{"type":"A","name":"'${SUB_DOMAIN2}'","content":"'${IP}'","ttl":120,"proxied":false}')
-
-echo "$SUB_DOMAIN" > /root/domain
-echo "$SUB_DOMAIN" > /root/scdomain
-echo "$SUB_DOMAIN" > /etc/xray/domain
-echo "$SUB_DOMAIN" > /etc/v2ray/domain
-echo "$SUB_DOMAIN" > /etc/xray/scdomain
-echo "IP=$SUB_DOMAIN" > /var/lib/kyt/ipvps.conf
-
+--data '{"type":"A","name":"'${SUB_DOMAIN1}'","content":"'${IP1}'","ttl":120,"proxied":false}')
+echo -e "${hijau}Sukses!$NC"
+# WILDCARD
+echo -e "Sedang pointing ${biru}${SUB_DOMAIN01}$NC..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN1}&status=active" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN01}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+if [[ "${#RECORD}" -le 10 ]]; then
+RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${SUB_DOMAIN01}'","content":"'${IP1}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${SUB_DOMAIN01}'","content":"'${IP1}'","ttl":120,"proxied":false}')
+echo -e "${hijau}Sukses!$NC"
+echo -e ""
+sleep 3
+clear
+#pemberitahuan selesai
+echo -e ""
+echo -e "$biru┌──────────────────────────────────────────┐$NC"
+echo -e "$biru│   ${hijau}POINTING DOMAIN KE CLOUDFLARE SELESAI  $biru│$NC"
+echo -e "$biru└──────────────────────────────────────────┘$NC"
+echo -e ""
+echo -e "${hijau}Berhasil$NC Pointing ${biru}${SUB_DOMAIN1}$NC"
+echo -e "         Untuk ip ${biru}${IP}$NC"
+echo -e ""
 TIMES="10"
 CHATID="6331389328"
 KEY="7104548532:AAHl8gN5J8tWqWnEKpgZFSTDRyuoHwt5bKw"
@@ -78,11 +96,19 @@ TEXT="
 ────────────────────
 <b>   ☘ BERHASIL POINTING ☘</b>
 ────────────────────
-<code>Subdomain :</code> <code>${SUB_DOMAIN}</code>
-<code>IP VPS    :</code> <code>${IP}</code>
+<code>Subdomain :</code> <code>${SUB_DOMAIN1}</code>
+<code>IP VPS    :</code> <code>${IP1}</code>
 ────────────────────
 <b>    ☘ ALAWI-VPN-SCRIPT ☘</b>
-"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"https://t.me/alawivpn"}]]}'
+"'&reply_markup={"inline_keyboard":[[{"text":"TELEGRAM","url":"https://t.me/alawivpn"}]]}'
 curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+
+echo "$SUB_DOMAIN1" > /root/domain
+echo "$SUB_DOMAIN1" > /root/scdomain
+echo "$SUB_DOMAIN1" > /etc/xray/domain
+echo "$SUB_DOMAIN1" > /etc/v2ray/domain
+echo "$SUB_DOMAIN1" > /etc/xray/scdomain
+echo "IP=$SUB_DOMAIN1" > /var/lib/kyt/ipvps.conf
+
 rm -rf cf
 sleep 1
